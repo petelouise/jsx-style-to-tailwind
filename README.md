cc90a8088274a39c257d0f2d4fe33860334e0078
Pete Brooks <pete@txidigital.com>
2024-02-03 01:46:01 -0600

    WIP

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # jsx-style-to-tailwind
+
 Convert JSX styles to Tailwind classes.
--- index.js
+++ index.js
@@ -1,58 +1,81 @@
-import fs from 'fs';
-import { parse } from '@babel/parser';
-import traverse from '@babel/traverse';
-import * as t from '@babel/types';
-import generate from '@babel/generator';
-import { CssToTailwind } from 'css2tailwind';
+#!/usr/bin/env node
+
+const fs = require("fs")
+const { parse } = require("@babel/parser")
+const traverse = require("@babel/traverse").default
+const t = require("@babel/types")
+const generate = require("@babel/generator").default
+const { CssToTailwind } = require("css2tailwind")
 
 // Utility function to convert style object to CSS string
 const toCSSString = (styles) => {
   return Object.entries(styles).reduce((css, [key, value]) => {
-    const cssKey = key.replace(/[A-Z]/g, (m) => "-" + m.toLowerCase());
-    return `${css}${cssKey}: ${value}; `;
-  }, '');
-};
+    const cssKey = key.replace(/[A-Z]/g, (m) => "-" + m.toLowerCase())
+    return `${css}${cssKey}: ${value}; `
+  }, "")
+}
 
 // Converts CSS to Tailwind classes using css2tailwind
 const convertCSSToTailwind = (cssString) => {
-  const conversionResult = CssToTailwind(cssString);
-  return conversionResult.data.map(({ resultVal }) => resultVal).join(' ');
-};
+  const conversionResult = CssToTailwind(cssString)
+  return conversionResult.data.map(({ resultVal }) => resultVal).join(" ")
+}
 
 // Main function to convert inline styles to Tailwind classes in a React component
 const convertComponentStylesToTailwind = async (filePath) => {
-  const code = fs.readFileSync(filePath, 'utf8');
+  const code = fs.readFileSync(filePath, "utf8")
   const ast = parse(code, {
-    sourceType: 'module',
-    plugins: ['jsx'],
-  });
+    sourceType: "module",
+    plugins: ["jsx"],
+  })
 
   traverse(ast, {
     JSXAttribute(path) {
-      if (path.node.name.name === 'style' && t.isJSXExpressionContainer(path.node.value)) {
-        const styles = path.node.value.expression.properties.reduce((acc, prop) => {
-          acc[prop.key.name || prop.key.value] = prop.value.value || prop.value.expression.value;
-          return acc;
-        }, {});
+      if (
+        path.node.name.name === "style" &&
+        t.isJSXExpressionContainer(path.node.value)
+      ) {
+        const styles = path.node.value.expression.properties.reduce(
+          (acc, prop) => {
+            acc[prop.key.name || prop.key.value] =
+              prop.value.value || prop.value.expression.value
+            return acc
+          },
+          {},
+        )
 
-        const cssString = toCSSString(styles);
-        const tailwindClasses = convertCSSToTailwind(`.component {${cssString}}`);
+        const cssString = toCSSString(styles)
+        const tailwindClasses = convertCSSToTailwind(
+          `.component {${cssString}}`,
+        )
 
         path.replaceWith(
           t.jsxAttribute(
-            t.jsxIdentifier('className'),
-            t.stringLiteral(tailwindClasses)
-          )
-        );
+            t.jsxIdentifier("className"),
+            t.stringLiteral(tailwindClasses),
+          ),
+        )
       }
     },
-  });
+  })
 
-  const { code: transformedCode } = generate(ast);
+  const { code: transformedCode } = generate(ast)
 
-  // Output the transformed code or write it back to a file
-  console.log(transformedCode);
-};
+  return transformedCode
+}
 
-// Example usage
-// convertComponentStylesToTailwind('path/to/your/ReactComponent.js');
+function handleCliInput() {
+  const filePath = process.argv[2]
+  if (!filePath) {
+    console.error("Error: Please provide a file path as an argument.")
+    process.exit(1)
+  }
+  output = convertComponentStylesToTailwind(filePath)
+  console.log(output)
+}
+
+if (require.main === module) {
+  handleCliInput()
+}
+
+module.exports = { convertComponentStylesToTailwind }
